ac7a0b5c622dbbf63b20c52d0dd069c0e6424b38
nebularthm <mw376@duke.edu>
2022-07-17 09:11:24 -0400

    aadded in code for training the model and getting the aption from it, if only my computer could run it

--- backend/caption_model.py
+++ backend/caption_model.py
@@ -3,10 +3,13 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint, \
     TensorBoard
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
 import os
 import tensorflow as tf
 import string
+import pickle as pkl
+import psutil
 from pickle import dump
 from pickle import load
 from keras.applications.xception import Xception #to get pre-trained model Xception
@@ -25,24 +28,27 @@ from tqdm import tqdm
  #to check loop progress
 from keras import layers
 from data_munger import process_data
-
-
+MAXLEN = 500
+VOCAB_SIZE = 50
+OUTPUT_PATH = 'C:\\Users\\nebul\Desktop\\instahack\\ml_instagram_caption_generation\\backend\\output'
 def extract_features(df):
-    model = VGG16()
+    model = VGG16(include_top=True, weights='imagenet')
     model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    print("checking gpu")
+    print(tf.test.is_gpu_available())
     print(model.summary())
     features = {}
-    images_list = df['image']
+    images_list = df['Image File']
     img_path = 'backend/data/instadata/instagram_data'
     for i, name in tqdm(enumerate(images_list)):
-        filename = os.path.join(os.getcwd(), img_path, name + '.jpg')
-        image = load_img(filename,target_size=(224,224))
+        filename = os.path.join(os.getcwd(), img_path, f"{name}.jpg")
+        image = load_img(filename,target_size=(224,224,3))
         image = img_to_array(image)
-        image = image.reshape((1,image.shape[0],image.shape[1],image.shape[2]))
+        # image = image.reshape((1,image.shape[0],image.shape[1],image.shape[2]))
         image = preprocess_input(image)
-        feature = model.predict(image,verbose=0)
+        feature = model.predict(image.reshape((1,) + image.shape[:3]),verbose=0)
         image_id = name.split('.')[0]
-        features[image_id] = feature
+        features[image_id] = feature.flatten()
     return features
 
 
@@ -57,8 +63,8 @@ def preprocessing(dtexts, dimages):
     for text, image in zip(dtexts, dimages):
         for t in range(1, len(text)):
             in_text, out_text = text[:t], text[t]
-            in_text = pad_sequences([in_text], maxlen=240).flatten()
-            out_text = to_categorical(out_text, num_classes=5000)
+            in_text = pad_sequences([in_text], maxlen=MAXLEN).flatten()
+            out_text = to_categorical(out_text, num_classes=VOCAB_SIZE)
 
             Xtext.append(in_text)
             Ximage.append(image)
@@ -77,8 +83,8 @@ def img_to_text_model(input_shape):
     input_image = layers.Input(shape=(input_shape,))
     fimage = layers.Dense(256, activation='relu', name="ImageFeature")(
         input_image)
-    input_txt = layers.Input(shape=(240,))
-    ftxt = layers.Embedding(5000, embedding_dimension,
+    input_txt = layers.Input(shape=(MAXLEN,))
+    ftxt = layers.Embedding(VOCAB_SIZE, embedding_dimension,
                             mask_zero=True)(input_txt)
     ftxt = layers.LSTM(256, name="CaptionFeature")(ftxt)
     decoder = layers.add([ftxt, fimage])
@@ -93,9 +99,9 @@ def img_to_text_model(input_shape):
 
 def predict_caption(img_text_model, image, tokenizer, index_word):
     in_text = '<START>'
-    for iword in range(240):
+    for iword in range(MAXLEN):
         sequence = tokenizer.texts_to_sequences([in_text])[0]
-        sequence = pad_sequences([sequence], 240)
+        sequence = pad_sequences([sequence], MAXLEN)
         yhat = img_text_model.predict([image, sequence], verbose=0)
         yhat = np.argmax(yhat)
         new_word = index_word[yhat]
@@ -104,13 +110,63 @@ def predict_caption(img_text_model, image, tokenizer, index_word):
             break
     return in_text
 
+def metric(hist):
+    for label in ['loss', 'val_loss']:
+        plt.plot(hist.history[label], label=label)
+    plt.legend()
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.show()
+    plt.savefig(os.path.join(OUTPUT_PATH, 'metrics.png'))
+
+
+def plot_images(img_text_model, fnm_test, di_test, tokenizer, index_word):
+    npic = 5
+    npix = 224
+    target_size = (npix, npix, 3)
+    count = 1
+    fig = plt.figure(figsize=(10, 20))
+    for jpgfnm, image_feature in zip(fnm_test[:npic], di_test[:npic]):
+        # images
+        filename = os.path.join('data/instadata/instagram_data', jpgfnm)
+        image_load = load_img(filename, target_size=target_size)
+        ax = fig.add_subplot(npic, 2, count, xticks=[], yticks=[])
+        ax.imshow(image_load)
+        count += 1
+
+        # caption
+        caption = predict_caption(img_text_model,
+                                  image_feature.reshape(1, len(image_feature)),
+                                  tokenizer,
+                                  index_word)
+        ax = fig.add_subplot(npic, 2, count)
+        plt.axis('off')
+        ax.plot()
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        ax.text(0, 0.5, caption, fontsize=20)
+        count += 1
+    print('[INFO] displaying sample results')
+    plt.show()
+    plt.savefig(os.path.join(OUTPUT_PATH, 'sample_output.png'))
+
+
 def train_model():
 
+    firstcsv = r"C:\\Users\\nebul\Desktop\\instahack\\ml_instagram_caption_generation\backend\data\\instadata\instagram_data\\captions_csv.csv"
+    secondcsv = r"C:\\Users\\nebul\Desktop\\instahack\\ml_instagram_caption_generation\backend\data\\instadata\instagram_data\\captions_csv2.csv"
 
-    df = process_data('/Users/neb/Desktop/hackathon/ml_instagram_caption_generation/backend/data/instadata/instagram_data/captions_csv.csv', '/Users/neb/Desktop/hackathon/ml_instagram_caption_generation/backend/data/instadata/instagram_data/captions_csv2.csv')
-
-    images = extract_features(df)
-    prop_test, prop_val = 0.2, 0.2
+    df = process_data(firstcsv)
+    images = []
+    fname = f"{OUTPUT_PATH}\\small_image_features_dictionary.pkl"
+    try:
+        images = pkl.load(open(fname, 'rb'))
+    except:
+        images = extract_features(df)
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
+    pkl.dump(images, open(fname, 'wb'),
+              protocol=pkl.HIGHEST_PROTOCOL)
+    prop_test, prop_val = 0.1, 0.2
 
     N = df.shape[0]
     Ntest, Nval = int(N * prop_test), int(N * prop_val)
@@ -120,7 +176,7 @@ def train_model():
                 data_list[Ntest:Ntest + Nval],
                 data_list[Ntest + Nval:])
 
-    dt_test, dt_val, dt_train = split_test_val_train(df['caption'], Ntest, Nval)
+    dt_test, dt_val, dt_train = split_test_val_train(df['Caption'], Ntest, Nval)
     images = list(images.items())
     images = np.asarray(images)
 
@@ -128,9 +184,13 @@ def train_model():
                                                Nval)
     fnm_test, fnm_val, fnm_train = split_test_val_train(images[:, 0], Ntest,
                                                         Nval)
+    global MAXLEN
+    MAXLEN = np.max([len(text) for text in df['Caption']])
     nb_words = 5000
     tokenizer = Tokenizer(num_words=nb_words, oov_token='<OOV>')
-    tokenizer.fit_on_texts(df['caption'][Ntest:])
+    tokenizer.fit_on_texts(df['Caption'][Ntest:])
+    global VOCAB_SIZE
+    VOCAB_SIZE = len(tokenizer.word_index) + 1
     dt_train = tokenizer.texts_to_sequences(dt_train)
     dt_test = tokenizer.texts_to_sequences(dt_test)
     dt_val = tokenizer.texts_to_sequences(dt_val)
@@ -159,14 +219,22 @@ def train_model():
 
     start = time.time()
 
+    checkpoint_path = os.path.join(OUTPUT_PATH, "checkpoints",
+                                   'cp-{epoch:02d}-{val_loss:.2f}.hdf5')
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+
     es = EarlyStopping(monitor='val_loss', patience=3,
                        restore_best_weights=True)
+    mc = ModelCheckpoint(filepath=checkpoint_path, monitor='val_loss',
+                         save_best_only=False, save_weights_only=True)
+    tb = TensorBoard(log_dir=f'{OUTPUT_PATH}\\logs', histogram_freq=1, write_graph=True,
+                     embeddings_freq=1)
 
     hist = img_text_model.fit(train_dataset,
                               epochs=20, verbose=2,
                               batch_size=32,
                               validation_data=val_dataset,
-                              callbacks=[es])  # pass callback to
+                              callbacks=[es,mc,tb])  # pass callback to
 
     end = time.time()
 
@@ -177,13 +245,31 @@ def train_model():
     del Xtext_val
     del ytext_val
 
+    metric(hist)
+
     index_word = dict([(index, word) for word, index in tokenizer.word_index])
+    plot_images(img_text_model, fnm_test, di_test, tokenizer, index_word)
     Xtext_test, Ximage_test, ytext_test = preprocessing(dt_test, di_test)
-    img_text_model.save('output/models/small_model.h5')
+    fname = f'{OUTPUT_PATH}\\models\\small_model.h5'
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
+    img_text_model.save(fname)
+def get_caption(image):
+    nb_words = 5000
+    tokenizer = Tokenizer(num_words=nb_words, oov_token='<OOV>')
+    index_word = dict([(index, word) for word, index in tokenizer.word_index])
+    fname = f'{OUTPUT_PATH}\\models\\small_model.h5'
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
+    modelCaption = load_model(fname)
+    image_feature = None
+    model = VGG16(include_top=True, weights='imagenet')
+    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    image = load_img(image,target_size=(224,224,3))
+    image = img_to_array(image)
+    image = image.reshape((1,image.shape[0],image.shape[1],image.shape[2]))
+    image = preprocess_input(image)
+    image_feature = model.predict(image.reshape((1,) + image.shape[:3]),verbose=0).flatten()
+    return predict_caption(modelCaption, image_feature, tokenizer,index_word )
+
 
 if __name__ == '__main__':
     train_model()
-
-
-
-
--- backend/data_munger.py
+++ backend/data_munger.py
@@ -3,27 +3,36 @@ import numpy as np
 
 non_alpha_num = ['!', '@', '#', '$', '%', '&', '*', '"', "'", ".", ",", '?', ';', ":"]
 
-
+def addCaptionTags(captions):
+    caps = []
+    for txt in captions:
+        txt = '<START>' + txt + "<END>"
+        caps.append(txt)
+    return caps
 # Provide file paths as parameter.
 # Returns a DataFrame with all the image locations and captions
 def process_data(*file_paths):
-    global non_alpha_num
-    df = {"image": list(),
-          "caption": list()}
-    for file_path in file_paths:
-        captions_csv = pd.read_csv(file_path, sep=',')
-        columns = list(captions_csv.columns)
-        if columns[0] == 'Sr No':
-            captions_csv.drop(columns=['Sr No'], inplace=True)
-        columns = list(captions_csv.columns)
-        for index, row in captions_csv.iterrows():
-            caption = row[columns[1]]
-            if caption is np.nan:
-                continue
-            elif all([char in caption for char in non_alpha_num]):
-                for char in non_alpha_num:
-                    if char in caption:
-                        caption.replace(char, "")
-            df['image'].append(row[columns[0]])
-            df['caption'].append(caption)
-    return pd.DataFrame(df)
+    # global non_alpha_num
+    # df = {"image": list(),
+    #       "caption": list()}
+    # for file_path in file_paths:
+    #     captions_csv = pd.read_csv(file_path, sep=',')
+    #     columns = list(captions_csv.columns)
+    #     if columns[0] == 'Sr No':
+    #         captions_csv.drop(columns=['Sr No'], inplace=True)
+    #     columns = list(captions_csv.columns)
+    #     for index, row in captions_csv.iterrows():
+    #         caption = row[columns[1]]
+    #         if caption is np.nan:
+    #             continue
+    #         elif all([char in caption for char in non_alpha_num]):
+    #             for char in non_alpha_num:
+    #                 if char in caption:
+    #                     caption.replace(char, "")
+    #         df['image'].append(row[columns[0]])
+    #         caption = addCaptionTags(caption)
+    #         df['caption'].append(caption)
+    df = pd.read_csv(file_paths[0], sep=',', encoding='utf-8', header=0,index_col=0)
+    df.dropna(axis=0, how='any', inplace=True)
+    df['Caption'] = addCaptionTags(df['Caption'])
+    return df
